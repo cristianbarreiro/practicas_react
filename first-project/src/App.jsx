7946a158cc47aafa342200bb0100aa84babf23de
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-12-17 00:44:48 -0300

    feat: usuarios renderizados

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -4,19 +4,42 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 function App() {
 
+    const users = [
+        {
+            userName: 'midudev',
+            name: 'Miguel Ángel Durán',
+            isFollowing: true
+        },
+        {
+            userName: 'pheralb',
+            name: 'Pablo Hernández',
+            isFollowing: false
+        },
+        {
+            userName: 'cristianbarreiro',
+            name: 'Cristian Barreiro',
+            isFollowing: false
+        },
+        {
+            userName: 'TMChein',
+            name: 'Tomas',
+            isFollowing: false
+        }
+    ]
+
     return (
         <section className="App">
-            <TwitterFollowCard userName="midudev" initialIsFollowing={true}>
-                Miguel Ángel Durán
-            </TwitterFollowCard>
-
-            <TwitterFollowCard userName="pheralb" initialIsFollowing={false}>
-                Pablo Hernández
-            </TwitterFollowCard>
-
-            <TwitterFollowCard userName="cristianbarreiro" initialIsFollowing={false}>
-                Cristian Barreiro
-            </TwitterFollowCard>
+            {users.map(user => {
+                const { userName, name, isFollowing } = user;
+                return (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}>
+                        {name}
+                    </TwitterFollowCard>
+                );
+            })}
         </section>
     )
 }
