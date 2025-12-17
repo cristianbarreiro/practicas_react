23482ef7cdd4fc0a9b8f26f7f2938763ab2817a9
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-12-16 23:16:24 -0300

    feat

--- first-project/src/TwitterFollowCard.jsx
+++ first-project/src/TwitterFollowCard.jsx
@@ -31,7 +31,8 @@ export function TwitterFollowCard({ children, userName, initialIsFollowing}) {
 
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-stopFollowing">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
