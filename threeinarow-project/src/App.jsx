9d7d91963f53c8d50cb5dd531c60ef418b24bf28
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-12-21 03:53:08 -0300

    feat: localStorage

--- threeinarow-project/src/App.jsx
+++ threeinarow-project/src/App.jsx
@@ -8,8 +8,16 @@ import { checkWinnerFrom, checkEndGame } from './logic/board.js';
 import { WinnerModal } from './components/WinnerModal.jsx';
 
 function App() {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [turn, setTurn] = useState(TURNS.X);
+    const [board, setBoard] = useState(() => {
+        const boardFromStorage = window.localStorage.getItem('board');
+        if (boardFromStorage) return JSON.parse(boardFromStorage);
+        return Array(9).fill(null);
+    });
+
+    const [turn, setTurn] = useState(() => {
+        const turnFromStorage = window.localStorage.getItem('turn');
+        return turnFromStorage ?? TURNS.X;
+    });
     // null = no hay ganador, false = empate
     const [winner, setWinner] = useState(null);
 
@@ -17,6 +25,9 @@ function App() {
         setBoard(Array(9).fill(null));
         setTurn(TURNS.X);
         setWinner(null);
+
+        window.localStorage.removeItem('board');
+        window.localStorage.removeItem('turn');
     }
 
     const updateBoard = (index) => {
@@ -29,6 +40,11 @@ function App() {
         //cambiar el turno
         const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
         setTurn(newTurn);
+        //guardar partida
+        window.localStorage.setItem('board', JSON.stringify(newBoard));
+        window.localStorage.setItem('turn', newTurn);
+
+
         //revisar si hay ganador
         const newWinner = checkWinnerFrom(newBoard);
         if (newWinner) {
