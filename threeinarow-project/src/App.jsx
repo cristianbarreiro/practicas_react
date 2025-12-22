03cded44d6f251f8b17be242788e3821ff818074
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-12-22 02:45:38 -0300

    feat: update repo w/ third project

--- threeinarow-project/src/App.jsx
+++ threeinarow-project/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import confetti from 'canvas-confetti';
 import { Square } from './components/Square.jsx';
