b1c0aa462c15b6207a62979895cd46d6f2a56c79
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-12-22 03:01:28 -0300

    feat: cursor

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -4,6 +4,7 @@ const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  //pointer move
   useEffect(() => {
     console.log("effect", { enabled });
 
@@ -21,6 +22,15 @@ const FollowMouse = () => {
       console.log("cleanup", { enabled });
       window.removeEventListener("pointermove", handleMove);
     };
+  }, [enabled]);
+
+  //change body classsname
+  useEffect(() => {
+   document.body.classList.toggle('no-cursor', enabled);
+
+   return () => {
+    document.body.classList.remove('no-cursor');
+   }
   }, [enabled]);
 
   return (
